197fd5518bf5406b75fe748f9ff68991a36c43c4
Denis Skarlat <denis.skarlat@quantron-systems.com>
2021-05-27 11:48:07 +0300

    Edit main.kt

--- kotlin_practice/src/main/kotlin/main.kt
+++ kotlin_practice/src/main/kotlin/main.kt
@@ -2,14 +2,24 @@ import dagger.internal.DaggerGenerated
 import javax.inject.Inject
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    launchDecorator()
+//    launchDagger()
+}
+
+private fun launchDecorator() {
+    val textView = TextViewComponent()
+    val scrollDecorator = ScrollDecorator(textView)
+    val borderDecorator = BorderDecorator(scrollDecorator)
+    borderDecorator.draw()
+}
+
+private fun launchDagger() {
     println(DaggerAppComponent.create().getCat().person.name)
     DaggerCoffeeApp_CoffeeShop.create().maker().brew()
-    println( DaggerCoffeeApp_CoffeeShop.create().logger().logs())
+    println(DaggerCoffeeApp_CoffeeShop.create().logger().logs())
     println("SomeClass " + DaggerAppComponent.create().getSome().person.name)
 //    println("SomeClass2 " + DaggerAppComponent.create().getSome2().someClass.name)
     println("SomeClass2 " + DaggerAppComponent.create().getSome2())
-
 }
 
 class SomeClass @Inject constructor(val person: Person) {
@@ -17,7 +27,8 @@ class SomeClass @Inject constructor(val person: Person) {
 }
 
 class SomeClass2 @Inject constructor(val someClass2: SomeClass) {
-    @Inject lateinit var someClass: SomeClass
+    @Inject
+    lateinit var someClass: SomeClass
 
     /*init {
         DaggerAppComponent.create().inject(this)
